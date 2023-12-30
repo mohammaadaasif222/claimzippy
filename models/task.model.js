31af5881dde4f584c77645b79be1884de13dc2a2
Mohammad Aasif <kasifsaif784@gmail.com>
2023-12-30 08:58:44 +0530

    add validation

--- models/task.model.js
+++ models/task.model.js
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
-    title: String,
-    description: {
+    title: {
         type: String,
+        required: true,
+        minlength: 1, 
+        maxlength: 50
+      },
+      description: {
+        type: String,
+        required: true,
+        minlength: 1, 
         maxlength: 200
-    }
+      }
 });
 
 const Task = mongoose.model("Task",taskSchema);
